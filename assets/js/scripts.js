b27bc69553896a4ae1c7f72a3da1db3b62c53dc8
michellejt <mtongas@gmail.com>
2022-08-17 01:34:53 +0930

    css

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -58,7 +58,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
-$(document).ready(function () {
+$j(document).ready(function () {
     ('.category:contains("editorial")').parent().addClass("editorial");
   });
 

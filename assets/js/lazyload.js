0ff9881bbf28ce2485228f561b8fb800bc71c45d
michellejt <mtongas@gmail.com>
2023-02-12 17:54:37 +1030

    Update lazyload.js

--- assets/js/lazyload.js
+++ assets/js/lazyload.js
@@ -1,12 +1,14 @@
 const loadImages = document.querySelectorAll('img.lazy')
 
-const observer = new IntersectionObserver(observerHandler)
+const observer = new IntersectionObserver(observerHandler, {
+    threshold: 1.0
+})
 
 function observerHandler(entries, observer) {
   entries.forEach((entry) => {
      if (entry.intersectionRatio > 0) {
        entry.target.src = entry.target.dataset.src
-       //entry.target.classList.remove('lazy')
+       entry.target.classList.remove('lazy')
      } 
   });
 }

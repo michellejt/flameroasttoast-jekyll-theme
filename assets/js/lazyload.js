3d24c30196c5c1c6cd0e319e2d5c7986d9c36883
michellejt <mtongas@gmail.com>
2023-03-01 22:40:21 +1030

    Update lazyload.js

--- assets/js/lazyload.js
+++ assets/js/lazyload.js
@@ -1,7 +1,7 @@
 const loadImages = document.querySelectorAll('img.lazy')
 
 const observer = new IntersectionObserver(observerHandler, {
-    threshold: 0.5
+    threshold: 0.2
 })
 
 function observerHandler(entries, observer) {
